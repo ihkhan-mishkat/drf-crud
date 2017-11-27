b523e8a4f5ddec1139b54fe89b6a65882194db73
I.H. Khan Mishkat <ihkhan.mishkat@gmail.com>
2017-11-27 18:43:08 +0600

    ModelTestCase added

--- api/tests.py
+++ api/tests.py
@@ -1,6 +1,18 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
 from django.test import TestCase
+from .models import Contactlist
 
-# Create your tests here.
+class ModelTestCase(TestCase):
+    """This class defines the test suite for the contactlist model."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.contactlist_name = "Write world class code"
+        self.contactlist = Contactlist(name=self.contactlist_name)
+
+    def test_model_can_create_a_contactlist(self):
+        """Test the contactlist model can create a contactlist."""
+        old_count = Contactlist.objects.count()
+        self.contactlist.save()
+        new_count = Contactlist.objects.count()
+        self.assertNotEqual(old_count, new_count)
